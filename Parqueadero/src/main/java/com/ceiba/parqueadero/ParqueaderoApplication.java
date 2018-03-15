344f8584b6a4f88456d9390f98368aea2fa89f0d
Nathally04 <anggie.naty@gmail.com>
2018-03-15 08:21:10 -0500

    se renombra el proyecto y los paquetes

--- Parqueadero/src/main/java/com/ceiba/parqueadero/ParqueaderoApplication.java
+++ Parqueadero/src/main/java/com/ceiba/parqueadero/ParqueaderoApplication.java
@@ -1,4 +1,4 @@
-package com.ceiba.Parqueadero;
+package com.ceiba.parqueadero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
