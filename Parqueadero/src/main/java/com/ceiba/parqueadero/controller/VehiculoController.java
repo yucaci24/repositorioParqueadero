bf525faf0ca15524fde0be53cc44af31ad2b423d
Nathally04 <anggie.naty@gmail.com>
2018-04-11 11:46:58 -0500

    .

--- Parqueadero/src/main/java/com/ceiba/parqueadero/controller/VehiculoController.java
+++ Parqueadero/src/main/java/com/ceiba/parqueadero/controller/VehiculoController.java
@@ -32,11 +32,11 @@ public class VehiculoController {
 	@RequestMapping(value = "/ingresos", method = RequestMethod.POST, headers = "Accept=application/json")
 	public ResponseEntity<?> ingresarVehiculo(@RequestBody Vehiculo vehiculo, UriComponentsBuilder uriComponentsBuilder, boolean estado){
 		if (vehiculo.getPlaca()==(null) || vehiculo.getPlaca().isEmpty() ) {
-			return new ResponseEntity<Object>(HttpStatus.NO_CONTENT);
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
 		}
 		
 		if ((vehiculoService.consultarVehiculoPorPlaca(vehiculo.getPlaca())!= null)) {
-			return new ResponseEntity<Object>(HttpStatus.NO_CONTENT);
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
 		}
 		
 		try {
@@ -59,11 +59,11 @@ public class VehiculoController {
 	@RequestMapping (value = "/salidas/{placa}", method = RequestMethod.PATCH, headers = "Accept=application/json")
 	public ResponseEntity<Vehiculo> salirVehiculo (@PathVariable("placa") String placa, @RequestBody Vehiculo vehiculo) {
 		if (placa == null) {
-			return new ResponseEntity<Vehiculo>(HttpStatus.NOT_FOUND);
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
 		}
 		Vehiculo vehiculoFuera = vehiculoService.consultarVehiculoPorPlaca(placa);
 		if (vehiculoFuera==null) {
-			return new ResponseEntity<Vehiculo>(HttpStatus.NO_CONTENT);
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
 		}
 		vehiculoFuera.setEstado(vehiculo.isEstado());
 		vehiculoService.salirVehiculo(vehiculoFuera);
@@ -75,7 +75,7 @@ public class VehiculoController {
 	public ResponseEntity<Vehiculo> consultarVehiculo(@PathVariable("placa")String placa){
 		Vehiculo vehiculo = vehiculoService.consultarVehiculoPorPlaca(placa);
 			if (placa == null) {
-					return new ResponseEntity<Vehiculo>(HttpStatus.NOT_FOUND);
+					return new ResponseEntity(HttpStatus.NOT_FOUND);
 			}
 			return new ResponseEntity<Vehiculo>(vehiculo, HttpStatus.OK);
 	}
