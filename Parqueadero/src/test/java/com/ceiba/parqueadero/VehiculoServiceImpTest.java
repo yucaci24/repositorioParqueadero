ef0f4f22c1abbfeb43024fe5b03ff6c6fdc5bb47
Nathally04 <anggie.naty@gmail.com>
2018-04-30 16:47:17 -0500

    .

---
+++ Parqueadero/src/test/java/com/ceiba/parqueadero/VehiculoServiceImpTest.java
@@ -0,0 +1,35 @@
+package com.ceiba.parqueadero;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ceiba.parqueadero.model.Vehiculo;
+import com.ceiba.parqueadero.service.VehiculoService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VehiculoServiceImpTest {
+	
+	@Autowired
+	VehiculoService validaciones;
+	
+	
+//		VehiculoServiceImpl validaciones = new VehiculoServiceImpl();
+	@Test
+	public void ComprobarConsultaOk () {
+		//Arrange
+		String placa = "qqq666";
+		
+		//Act
+		Vehiculo consultaOk = validaciones.consultarVehiculoPorPlaca(placa);
+		
+		//Assert
+		assertEquals(placa, consultaOk.placa);
+	}
+
+}
