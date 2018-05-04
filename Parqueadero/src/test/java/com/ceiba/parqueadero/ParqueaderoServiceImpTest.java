d7de9422f0c82346d8a73a0e9d2188233f312841
Nathally04 <anggie.naty@gmail.com>
2018-05-04 14:44:49 -0500

    .

--- Parqueadero/src/test/java/com/ceiba/parqueadero/ParqueaderoServiceImpTest.java
+++ Parqueadero/src/test/java/com/ceiba/parqueadero/ParqueaderoServiceImpTest.java
@@ -4,15 +4,10 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import com.ceiba.parqueadero.service.ParqueaderoService;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class ParqueaderoServiceImpTest {
 	
 	@Autowired
